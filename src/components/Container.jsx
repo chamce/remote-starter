72c001ebcfc0245a199700fce687413bc01b35bb
Chamce <chancellor.whitaker@eku.edu>
2023-04-13 15:01:47 -0400

    danger

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -36,7 +36,7 @@ export const Container = ({ children }) => {
             id="sidebarMenu"
             className="col-md-3 col-lg-2 d-md-block bg-body-tertiary sidebar collapse"
           >
-            <div className="position-sticky pt-3 sidebar-sticky">
+            <div className="bg-danger position-sticky pt-3 sidebar-sticky">
               <ul className="nav flex-column">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="/#">
