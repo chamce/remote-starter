420b9d2699777088e40834760a78cc77449c3b47
Chamce <chancellor.whitaker@eku.edu>
2023-04-25 15:14:37 -0400

    notes

--- src/main.jsx
+++ src/main.jsx
@@ -8,4 +8,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-// can we get font face included into wrapper css?
+/*
+- can we get font face included into wrapper css?
+
+- is it for bootstrap & styled-jsx to be set to external deps for the wrapper (not bundled)?
+
+- is it okay to just concatenate the two stylesheets as strings contrary to the docs?
+
+- styles defined in the host app could overwrite styles in the remote--is this okay? are we supposed to define styles outside of components with the default export css to avoid style clashing? does the global definition change any of these circumstances?
+*/
