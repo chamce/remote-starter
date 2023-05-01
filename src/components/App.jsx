68fedab3e2c5f2fe981bac786f828c0a311710d5
Chamce <chancellor.whitaker@eku.edu>
2023-05-01 13:39:15 -0400

    sidebar; bs styling is hard-coded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,5 @@ import "../styles/App.css";
 import { Wrapper } from "../remote/components/Wrapper";
 
 export const App = () => {
-  return (
-    <Wrapper>
-      <h1 className="text-center text-bg-light border border-dark shadow-sm">Example</h1>
-    </Wrapper>
-  );
+  return <Wrapper />;
 };
